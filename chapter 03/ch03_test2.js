92c85bc31608074b3d16e7217ab946859901d838
hyeji1221 <lhg2615@naver.com>
2020-08-13 02:46:12 +0900

    객체 속성에 함수 추가

--- chapter 03/ch03_test2.js
+++ chapter 03/ch03_test2.js
@@ -7,7 +7,7 @@ Person.add = function(a, b) {
 
 console.log('더하기 : %d', Person.add(10,10));
 
-//변수에 함수를 할당한 후 그 변수를 갹체의 속성으로 추가
+//변수에 함수를 할당한 후 그 변수를 객체의 속성으로 추가
 var oper = function(a, b) {
     return a+b;
 };
