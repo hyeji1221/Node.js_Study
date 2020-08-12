011ce750b2dd1bc70930828e98a47f591922f666
hyeji1221 <lhg2615@naver.com>
2020-08-12 19:25:41 +0900

    프로세스 객체 env

---
+++ chapter 02/ch02_test2.js
@@ -0,0 +1,4 @@
+console.dir(process.env);
+console.log('OS 환경 변수의 값 : '+process.env['OS']);
+// PC 환경 변수 값이 모두 출력되고 마지막에 OS 환경 변수의 값을 보여주는 한줄이 더 추가된다.
+//proces.env 속성을 사용하면 사용자 정의 환경 변수 뿐만 아니라 os 같은 시스템 환경 변수도 접근 가능하다.
