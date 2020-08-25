8c1d32176bbf25e123c1143bdb3dd008d9f518b7
hyeji1221 <lhg2615@naver.com>
2020-08-25 16:07:12 +0900

    파일 읽고 쓰기

---
+++ chapter 04/ch04_test8.js
@@ -0,0 +1,18 @@
+// output.txt 파일을 읽어 들인 후 output2.txt 파일로 쓰기
+
+var fs = require('fs');
+
+var infile = fs.createReadStream('./output.txt', {flags : 'r'});
+var outfile = fs.createWriteStream('./output2.txt', {flags : 'w'});
+
+infile.on('data', function(data){
+    console.log('읽어 들인 데이터', data);
+    outfile.write(data);
+});
+
+infile.on('end', function(){
+    console.log('파일 읽기 종료.');
+    outfile.end(function(){
+        console.log('파일 쓰기 종료');
+    });
+});
